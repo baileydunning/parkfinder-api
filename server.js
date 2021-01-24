1ee420e2d7795058755553ef2734704f4f459975
Bailey Dunning <bwdunning@gmail.com>
2021-01-23 17:54:28 -0700

    add dynamic route to specific parkCode

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const app = express()
 const cors = require('cors')
 const parkData = require('./parkData')
 
-
 app.set('port', process.env.PORT || 4000)
 app.use(express.json())
 app.use(cors())
@@ -19,7 +18,14 @@ app.get('/', (request, response) => {
   response.send(app.locals.title)
 })
 
-app.get('/parks', (request, response) => {
+app.get('/nationalParks', (request, response) => {
   const nationalParks = app.locals.nationalParks
   response.json({ nationalParks })
+})
+
+app.get('/nationalParks/:parkCode', (request, response) => {
+  const { parkCode } = request.params
+  const park = app.locals.nationalParks.find(park => park.parkCode === parkCode)
+
+  response.status(200).json(park)
 })
