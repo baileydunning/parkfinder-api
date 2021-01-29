ecdea853ed232cb75b26f8ae2bdf146f8c3e632b
Bailey Dunning <bwdunning@gmail.com>
2021-01-29 11:56:02 -0700

    add api call to express server

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require('cors')
+const request = require('request')
 const parkData = require('./parkData')
 
 const app = express()
@@ -8,6 +9,15 @@ app.set('port', process.env.PORT || 4000)
 app.use(express.json())
 app.use(cors())
 
+const makeApiCall = (url) => {
+  return new Promise((resolve, reject) => {
+    request(url, { json: true }, (err, res, body) => {
+      if (err) reject(err)
+      resolve(body)
+    })
+  })
+}
+
 app.locals.title = 'Parkfinder API'
 app.locals.nationalParks = parkData.national
 
@@ -15,20 +25,34 @@ app.listen(app.get('port'), () => {
   console.log(`${app.locals.title} is running on http://localhost:${app.get('port')}.`);
 })
 
-app.get('/', (request, response) => {
+app.get('/', (req, res) => {
   const nationalParks = app.locals.nationalParks
-  response.json({ nationalParks })
+  res.json({ nationalParks })
 })
 
-app.get('/:parkCode', (request, response) => {
-  const { parkCode } = request.params
+app.get('/api/:parkCode', (req, res) => {
+  const { parkCode } = req.params
+  const url = `https://developer.nps.gov/api/v1/parks?parkCode=${parkCode}&api_key=C7eGnlc0BVmtjubOSmuD8P6UPmcRfLM4ewoeNBuI`
   const park = app.locals.nationalParks.find(park => park.parkCode === parkCode)
-  
+
   if (!park) {
-    return response.sendStatus(404)
+    return res.sendStatus(404)
   }
 
-  response.status(200).json(park)
+  makeApiCall(url)
+    .then(response => res.json({ response }))
+    .catch(error => res.send(error))
+})
+
+app.get('/:parkCode', (req, res) => {
+  const { parkCode } = req.params
+  const park = app.locals.nationalParks.find(park => park.parkCode === parkCode)
+
+  if (!park) {
+    return res.sendStatus(404)
+  }
+
+  res.status(200).json(park)
 })
 
 app.get('/:parkCode/reviews', (request, response) => {
@@ -56,7 +80,7 @@ app.post('/:parkCode/reviews', (request, response) => {
     }
   }
 
-    const { name, parkReview } = review
-    park.reviews.push({ id, name, parkReview })
-    response.status(200).json(`Thanks for submitting a review for ${park.name}!`)
+  const { name, parkReview } = review
+  park.reviews.push({ id, name, parkReview })
+  response.status(200).json(`Thanks for submitting a review for ${park.name}!`)
 })
