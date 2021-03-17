c46288167b8c8725ee1ecd4d18e6da3f77b6a801
Bailey Dunning <bwdunning@gmail.com>
2021-03-17 14:44:03 -0600

    connect server to knex

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
+const environment = process.env.NODE_ENV || 'development'
+const configuration = require('./knexfile')[environment]
+const database = require('knex')(configuration)
 const express = require('express')
 const cors = require('cors')
 const request = require('request')
-const parkData = require('./parkData')
 const app = express()
 
 app.set('port', process.env.PORT || 4000)
@@ -18,29 +20,32 @@ const makeApiCall = (url) => {
 }
 
 app.locals.title = 'National Parkfinder API'
-app.locals.nationalParks = parkData.national
 
 app.listen(app.get('port'), () => {
   console.log(`${app.locals.title} is running on http://localhost:${app.get('port')}.`);
 })
 
-app.get('/', (req, res) => {
-  const nationalParks = app.locals.nationalParks
-  res.json({ nationalParks })
-})
-
-app.get('/:parkCode', (req, res) => {
-  const { parkCode } = req.params
-  const park = app.locals.nationalParks.find(park => park.parkCode === parkCode)
-
-  if (!park) {
-    return res.sendStatus(404)
+app.get('/api/v1/parks', async (req, res) => {
+  try {
+    const parks = await database('parks').select()
+    res.status(200).json(parks)
+  } catch(error) {
+    res.status(500).json({ error })
   }
-
-  res.status(200).json(park)
 })
 
-app.get('/api/:parkCode', (req, res) => {
+app.get('/api/v1/parks/:parkCode', async (req, res) => {
+  const { parkCode } = req.params
+  try {
+    const parks = await database('parks').select()
+    const foundPark = parks.find(park => park.parkCode === parkCode)
+    res.status(200).json(foundPark)
+  } catch (error) {
+    res.status(500).json({ error })
+  }
+})
+
+app.get('/api/v1/nps/:parkCode', (req, res) => {
   const { parkCode } = req.params
   const url = `https://developer.nps.gov/api/v1/parks?parkCode=${parkCode}&api_key=C7eGnlc0BVmtjubOSmuD8P6UPmcRfLM4ewoeNBuI`
   const park = app.locals.nationalParks.find(park => park.parkCode === parkCode)
@@ -54,7 +59,7 @@ app.get('/api/:parkCode', (req, res) => {
     .catch(error => res.send(error))
 })
 
-app.get('/api/alerts/:parkCode', (req, res) => {
+app.get('/api/v1/nps/alerts/:parkCode', (req, res) => {
   const { parkCode } = req.params
   const url = `https://developer.nps.gov/api/v1/alerts?parkCode=${parkCode}&api_key=C7eGnlc0BVmtjubOSmuD8P6UPmcRfLM4ewoeNBuI`
   const park = app.locals.nationalParks.find(park => park.parkCode === parkCode)
@@ -68,7 +73,7 @@ app.get('/api/alerts/:parkCode', (req, res) => {
     .catch(error => res.send(error))
 })
 
-app.get('/api/articles/:parkCode', (req, res) => {
+app.get('/api/v1/nps/articles/:parkCode', (req, res) => {
   const { parkCode } = req.params
   const url = `https://developer.nps.gov/api/v1/articles?parkCode=${parkCode}&api_key=C7eGnlc0BVmtjubOSmuD8P6UPmcRfLM4ewoeNBuI`
   const park = app.locals.nationalParks.find(park => park.parkCode === parkCode)
@@ -82,7 +87,7 @@ app.get('/api/articles/:parkCode', (req, res) => {
     .catch(error => res.send(error))
 })
 
-app.get('/api/campgrounds/:parkCode', (req, res) => {
+app.get('/api/v1/nps/campgrounds/:parkCode', (req, res) => {
   const { parkCode } = req.params
   const url = `https://developer.nps.gov/api/v1/campgrounds?parkCode=${parkCode}&api_key=C7eGnlc0BVmtjubOSmuD8P6UPmcRfLM4ewoeNBuI`
   const park = app.locals.nationalParks.find(park => park.parkCode === parkCode)
@@ -96,7 +101,7 @@ app.get('/api/campgrounds/:parkCode', (req, res) => {
     .catch(error => res.send(error))
 })
 
-app.get('/api/lessonplans/:parkCode', (req, res) => {
+app.get('/api/v1/nps/lessonplans/:parkCode', (req, res) => {
   const { parkCode } = req.params
   const url = `https://developer.nps.gov/api/v1/lessonplans?parkCode=${parkCode}&api_key=C7eGnlc0BVmtjubOSmuD8P6UPmcRfLM4ewoeNBuI`
   const park = app.locals.nationalParks.find(park => park.parkCode === parkCode)
@@ -110,7 +115,7 @@ app.get('/api/lessonplans/:parkCode', (req, res) => {
     .catch(error => res.send(error))
 })
 
-app.get('/api/places/:parkCode', (req, res) => {
+app.get('/api/v1/nps/places/:parkCode', (req, res) => {
   const { parkCode } = req.params
   const url = `https://developer.nps.gov/api/v1/places?parkCode=${parkCode}&api_key=C7eGnlc0BVmtjubOSmuD8P6UPmcRfLM4ewoeNBuI`
   const park = app.locals.nationalParks.find(park => park.parkCode === parkCode)
@@ -124,7 +129,7 @@ app.get('/api/places/:parkCode', (req, res) => {
     .catch(error => res.send(error))
 })
 
-app.get('/api/thingstodo/:parkCode', (req, res) => {
+app.get('/api/v1/nps/thingstodo/:parkCode', (req, res) => {
   const { parkCode } = req.params
   const url = `https://developer.nps.gov/api/v1/thingstodo?parkCode=${parkCode}&api_key=C7eGnlc0BVmtjubOSmuD8P6UPmcRfLM4ewoeNBuI`
   const park = app.locals.nationalParks.find(park => park.parkCode === parkCode)
@@ -138,7 +143,7 @@ app.get('/api/thingstodo/:parkCode', (req, res) => {
     .catch(error => res.send(error))
 })
 
-app.get('/api/webcams/:parkCode', (req, res) => {
+app.get('/api/v1/nps/webcams/:parkCode', (req, res) => {
   const { parkCode } = req.params
   const url = `https://developer.nps.gov/api/v1/webcams?parkCode=${parkCode}&api_key=C7eGnlc0BVmtjubOSmuD8P6UPmcRfLM4ewoeNBuI`
   const park = app.locals.nationalParks.find(park => park.parkCode === parkCode)
@@ -150,23 +155,4 @@ app.get('/api/webcams/:parkCode', (req, res) => {
   makeApiCall(url)
     .then(response => res.json({ response }))
     .catch(error => res.send(error))
-})
-
-app.post('/:parkCode/reviews', (request, response) => {
-  const { parkCode } = request.params
-  const park = app.locals.nationalParks.find(park => park.parkCode === parkCode)
-  const id = Date.now()
-  const review = request.body
-
-  for (let requiredParameter of ['name', 'parkReview']) {
-    if (!review[requiredParameter]) {
-      response
-        .status(422)
-        .send({ error: `Expected format: { name: <String>, parkReview: <String> }. You're missing a "${requiredParameter}" property.` });
-    }
-  }
-
-  const { name, parkReview } = review
-  park.reviews.push({ id, name, parkReview })
-  response.status(200).json(`Thanks for submitting a review for ${park.name}!`)
 })
